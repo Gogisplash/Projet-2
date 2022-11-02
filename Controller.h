bef6948b766d5a62d211d0f70f64282063f01738
mathis gasnier <mgasnier@gaming.tech>
2022-11-02 17:24:39 +0100

    a

--- Controller.h
+++ Controller.h
@@ -3,45 +3,38 @@
 class Controller
 {
 public:
+	enum
+	{
+		NONE,
+		DOWN,
+		UP,
+		PUSH,
+	};
 
-    enum
-    {
-        NONE,
-        DOWN,
-        UP,
-        PUSH,
-    };
+public:
+	Controller();
+	virtual ~Controller();
+
+	void Reset();
+	int GetKey(int key, bool press);
+
+	bool Back() { return m_back == DOWN; }
+	bool Start() { return m_start == DOWN; }
+	
+	bool Up() { return m_up; }
+	bool Down() { return m_down; }
+	bool Left() { return m_left; }
+	bool Right() { return m_right; }
+
+	void OnUpdate();
 
 public:
 
-    class Controller();
-    virtual ~Controller();
-
-    void Reset();
-    int GetKey(int key, bool press);
-
-    int Up() { return m_up; }
-    int Down() { return m_down; }
-    int Left() { return m_left; }
-    int Right() { return m_right; }
-
-    int Attack() { return m_attack == DOWN; }
-
-    int Pickup() { return m_pickup == DOWN; }
-
-    int Map() { return m_map == DOWN; }
-
-    void OnUpdate();
-
 protected:
-
-    bool m_up;
-    bool m_down;
-    bool m_left;
-    bool m_right;
-
-    bool m_attack;
-    bool m_pickup;
-    bool m_map;
-
+	int m_back;
+	int m_start;
+	bool m_up;
+	bool m_down;
+	bool m_left;
+	bool m_right;
 };
