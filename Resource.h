102f9bcd16546e0d2577b137f0b8485c7ea82a80
rkhan@gaming.tech <rkhan@gaming.tech>
2022-11-09 15:02:48 +0100

    1

--- Resource.h
+++ Resource.h
@@ -20,6 +20,7 @@
 #define IDB_TEXTURE_PLAYER_IDLE         130
 #define IDR_FONT1                       131
 #define IDR_WAVE_MENU                   133
+#define IDR_WAVE_DMENU                  134
 #define IDR_WAVE1                       136
 #define IDR_WAVE_CLICK1                 136
 #define IDB_PLAYER_RUN                  137
@@ -27,7 +28,7 @@
 #define IDB_TILESET                     138
 #define IDB_PNG1                        139
 #define IDB_GROUND                      139
-#define IDR_WAVE_DMENU					134
+#define IDB_TEXTURE_BG_LEVEL1           141
 #define IDB_TEXTURE_MENU                999
 #define IDC_TEXTURE_BACK                1000
 #define IDC_STATIC                      -1
@@ -37,7 +38,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
