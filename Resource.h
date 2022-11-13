d4ae09c4058ab8253fbd899f039116116a433a68
mathis gasnier <mgasnier@gaming.tech>
2022-11-13 18:39:39 +0100

    a

--- Resource.h
+++ Resource.h
@@ -28,6 +28,8 @@
 #define IDB_PNG1                        139
 #define IDB_GROUND                      139
 #define IDB_TEXTURE_BG_LEVEL1           141
+#define IDR_WAVE2                       142
+#define IDR_WAVE_GAME                   142
 #define IDB_TEXTURE_MENU                999
 #define IDC_TEXTURE_BACK                1000
 #define IDC_STATIC                      -1
@@ -37,7 +39,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_RESOURCE_VALUE        143
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
